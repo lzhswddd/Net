bc6fec2cf56ea9524bc936d9cf58d6c50f621769
lzh <897077371@qq.com>
2019-01-27 19:15:26 +0800

    finish

--- build.bat
+++ build.bat
@@ -1,21 +1,20 @@
 cmake ./
 @echo off
 echo search data.txt!
-if exist ./data.txt (    
+if exist "data.txt" (   
     echo data.txt is existence!
-) else (
-    echo data.txt is no find!\n
-    echo create data.txt...
-    if exist ./train_data (
-        python totxt.py
-        if exist ./data.txt ( 
-            echo create data.txt success!
-        )
-        else (
-            echo unknow error!
-        )
-    ) else (
-        echo error no find ./train_data...
-    )
+    exit
+) 
+echo data.txt is no find!
+echo create data.txt...
+if exist "train_data" (
+    python totxt.py
+    if exist ./data.txt ( 
+        echo create data.txt success!
+        exit
+    )  
+    echo ( unknow error! )
+    exit
 )
+echo error no find ./train_data...
 
